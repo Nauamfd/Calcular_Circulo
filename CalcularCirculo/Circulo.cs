cf9fa7c7a66f2216eea4ec3652d262467db2b74d
Nauamfd <nauamfd@gmail.com>
2023-04-19 15:17:55 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularCirculo/Circulo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularCirculo
+{
+    //Math.PI pegar o valor PI no método Math
+    public class Circulo : Programa
+    {
+        private double raio, perimetro, area;
+        private const double PI = Math.PI;
+       
+
+
+        //Pega os valores do cálculo do círculo
+        private static void pegarValores(double raio, double perimetro, double area) 
+        {
+            double RAIO = raio;
+            double PERIMETRO = perimetro;
+            double AREA = area;
+
+            Console.WriteLine(" \n " + $"Raio do círculo: {RAIO}" + " \n " + $"Perimetro do círculo: {PERIMETRO}" + " \n " + $"Área do círculo: {AREA}");
+        }
+        
+        //Calcula o perimetro e area do círculo
+        //Math.Pow usado para elevar o raio ao quadrado
+        public void calcularCirculo(double raio)
+        {
+            perimetro = 2 * PI * raio;
+            area = PI * Math.Pow(raio, 2);
+
+          
+            pegarValores(raio, perimetro, area);
+
+        }
+
+    }
+
+
+
+}
---
+++ CalcularCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿//Estudos constantes calcular círculo
+
+
+namespace CalcularCirculo
+{
+    public class Programa 
+    {
+        
+        static void Main(string[] args)
+        {
+            Circulo cr = new Circulo();
+            Console.WriteLine("Digite o valor da área do círculo:\n");
+            cr.calcularCirculo(Convert.ToDouble(Console.ReadLine()));
+
+            Console.ReadKey();
+       
+        }
+    }
+
+}
